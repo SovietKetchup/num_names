8397d75ec30dfeb053152fbd325659f9f2ae40ec
SovietKetchup <sam.comer+git@gmail.com>
2016-02-14 22:53:01 +0000

    Reformed the old hun_ten_uni to be much more readable

--- num_names.rb
+++ num_names.rb
@@ -40,89 +40,53 @@ class NumNames
 
   # Convert the number into
   def to_word
-    puts hun_ten_uni @num
+##    pos = -1
+##    num_words = " "
+##    @num.length.times {
+##        num_words += compile @num[pos]
+##      pos -= 1
+##    }
+##    num_words
   end
 
   private
-  # Sections of 3 -- hundreds, tens, units
-  def hun_ten_uni num_sep
-    c = -1
-    num_names = []
-    num_sep.length.times {
-      # No tens or hundreds
-      if num_sep[c].length == 1
-        # Applying the name of the number to a new array
-####################################
-## raise num_sep.inspect
-####################################
-        num_names[1] = @lookup[num_sep[c][num_sep[c].length -1]]
-      # No hundreds
-      elsif num_sep[c].length == 2
-        # If it's a 'teen' (or 10)
-        if num_sep[c][num_sep[c].length-2] == 1
-          num_names[1] = @lookup[num_sep[c][num_sep[c].length -2]] + @lookup[num_sep[c][num_sep[c].length -2]]
-        # Not a 'teen' i.e. 20, 21 etc
-        else
-          # Ends in 0 i.e. 20, 30, 40 etc
-          if num_sep[c][num_sep[c].length-1] == 0
-            num_names[1] = @lookup[num_sep[c][num_sep[c].length -2]]
-          # Not a 0
-          else
-            num_names[1] = @lookup[num_sep[c][num_sep[c].length -2]] + " " + @lookup[num_sep[c][num_sep[c].length -1]]
-          end
-        end
-      # Units tens and hundreds
-      else
-        # Hundreds is 0
-        if num_sep[c][num_sep[c].length-3] == "0"
-          # If it's a 'teen' (or 10)
-          if num_sep[c][num_sep[c].length-2] == 1
-            num_names[1] = digit_names(num_sep[c][num_sep[c].length-2] + num_sep[c][num_sep[c].length-1])
-          # Not a 'teen' i.e. 20, 21 etc
-          else
-            # Ends in 0 i.e. 20, 30, 40 etc
-            if num_sep[c][num_sep[c].length-1] == 0
-              num_names[1] = @lookup[ num_sep[c] [num_sep[c].length -2] ]
-            # Not a 0
-            else
-              num_names[1] = @lookup[num_sep[c][num_sep[c].length -2]] + " " + @lookup[num_sep[c][num_sep[c].length -1]]
-            end
-          end
-        # Hundreds has a value
-        else
-          # Tens and units are 0
-          if num_sep[c][num_sep[c].length-2] == 0 and num_sep[c][num_sep[c].length-1] == 0
-            num_names[1] = @lookup[num_sep[c][num_sep[c].length -3]] + " hundred"
-          # Tens or units have value
-          else
-            a = @lookup[num_sep[c][num_sep[c].length -3]] + " hundred"
-            # If it's a 'teen' (or 10)
-            if num_sep[c][num_sep[c].length-2] == 1
-              b = digit_names(num_sep[c][num_sep[c].length-2] + num_sep[c][num_sep[c].length-1])
-            # Not a 'teen' i.e. 20, 21 etc
-            else
-              # Ends in 0 i.e. 20, 30, 40 etc
-              if num_sep[c][num_sep[c].length-1] == 0
-                b = @lookup[num_sep[c][num_sep[c].length -2]]
-              # Not a 0
-              else
-                b = @lookup[num_sep[c][num_sep[c].length -2]] + " " + @lookup[num_sep[c][num_sep[c].length -2]]
-              end
-            end
-            num_names[1] = a + b
-          end
-        end
-      end
-      # The suffix (thousand, million etc)
-      case c
-        when -2 then num_names[1] += " thousand"
-        when -3 then num_names[1] += " million"
-        when -4 then num_names[1] += " billion"
-      end
-      c -= 1
-    }
-    num_names
+  # Sections of 3 -- units, tens, hundreds
+  def compile num_section
+    u = units num_section
+    t = tens num_section
   end
+
+  # Names for numbers in the units
+  def units n
+    unless n[-1] == "0"
+      if n[-2] == "1"
+        u = @lookup[ n[-2] + n[-1] ]
+      else
+        u = @lookup[ n[-1] ]
+      end
+    end
+    u
+  end
+
+  # Names for numbers in the tens
+  def tens n
+    unless n[-2].to_i > 1 or n[-2] == nil
+      t = @lookup[ n[-2] ]
+    end
+  end
+
+  # Names for numbers in the hundreds
+  def hundreds num
+    unless n[-3] == "0" or n[-3] == nil
+      if n[-1] == "0" and n[-2] == "0"
+        h = @lookup[ n[-3] ] + " hundred and"
+      else
+        h = @lookup[ n[-3] ] + " hundred"
+      end
+    end
+  end
+
+
 end
 
 # http://api.rubyonrails.org/classes/Array.html#method-i-in_groups_of
